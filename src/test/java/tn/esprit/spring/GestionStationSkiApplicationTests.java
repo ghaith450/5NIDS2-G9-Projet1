ffbdc6416c837a1b3e4c60ab6f2288268906e5cc
mustapha6969 <bazdeh.mustapha@esprit.tn>
2023-11-03 17:50:41 +0100

    xx

--- src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
+++ src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
@@ -5,19 +5,21 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.mockito.InjectMocks;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.Assertions;
+
 import java.util.Date;
 import java.util.Optional;
 
 import tn.esprit.spring.repositories.ISubscriptionRepository;
 import tn.esprit.spring.services.SubscriptionServicesImpl;
 import tn.esprit.spring.entities.Subscription;
-
-
+import tn.esprit.spring.entities.TypeSubscription;
 
 @SpringBootTest
-@ExtendWith(MockitoExtension.class)
+@ExtendWith({MockitoExtension.class, SpringExtension.class})
 class GestionStationSkiApplicationTests {
 
     @Mock
@@ -26,12 +28,12 @@ class GestionStationSkiApplicationTests {
     @InjectMocks
     SubscriptionServicesImpl subscriptionService;
 
-    Subscription sub = new Subscription(2.0, new Date(), new Date(), 55.0, TypeSubcription.ANNUAL);
+    Subscription sub = new Subscription(2.0, new Date(), new Date(), 55.0, TypeSubscription.ANNUAL);
 
     @Test
     public void testRetrieveSub() {
         Mockito.when(subscriptionRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(sub));
-        Subscription sub1 = subscriptionService.retrieveSubscriptionById("1");
+        Subscription sub1 = subscriptionService.retrieveSubscriptionById(1L);
         Assertions.assertNotNull(sub1);
     }
 }
