82a61ef2653351b1df5bf251e0105302ae6d45cd
mustapha6969 <bazdeh.mustapha@esprit.tn>
2023-11-03 18:05:16 +0100

    test

--- src/main/java/tn/esprit/spring/repositories/ISubscriptionRepository.java
+++ src/main/java/tn/esprit/spring/repositories/ISubscriptionRepository.java
@@ -11,7 +11,7 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Set;
 
-public interface ISubscriptionRepository extends CrudRepository<Subscription, int> {
+public interface ISubscriptionRepository extends CrudRepository<Subscription, Long> {
 
     @Query("select s from Subscription s where s.typeSub = :typeS order by s.startDate")
     Set<Subscription> findByTypeSubOrderByStartDateAsc(@Param("typeS") TypeSubscription typeSub);
