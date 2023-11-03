61a86be83fb76adca1f659024a86d13ea33e393b
mustapha6969 <bazdeh.mustapha@esprit.tn>
2023-11-03 17:27:13 +0100

    ajout classe de test

--- src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
+++ src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
@@ -1,13 +1,37 @@
 package tn.esprit.spring;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.ExtendWith;
+import org.mockito.InjectMocks;
+import java.util.Date;
+import java.util.Optional;
+
+import tn.esprit.spring.repositories.ISubscriptionRepository;
+import tn.esprit.spring.services.SubscriptionServicesImpl;
+import tn.esprit.spring.entities.Subscription;
+
+
 
 @SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class GestionStationSkiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Mock
+    ISubscriptionRepository subscriptionRepository;
 
+    @InjectMocks
+    SubscriptionServicesImpl subscriptionService;
+
+    Subscription sub = new Subscription(2.0, new Date(), new Date(), 55.0, TypeSubcription.ANNUAL);
+
+    @Test
+    public void testRetrieveSub() {
+        Mockito.when(subscriptionRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(sub));
+        Subscription sub1 = subscriptionService.retrieveSubscriptionById("1");
+        Assertions.assertNotNull(sub1);
+    }
 }
